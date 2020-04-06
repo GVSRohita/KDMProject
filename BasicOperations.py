4a845bb8c62f36b3ae17980312637218e18950e9
Rohita Goparaju <gvsrohita@gmail.com>
2020-04-06 16:33:04 -0500

    Added Parse tree

--- BasicOperations.py
+++ BasicOperations.py
@@ -3,6 +3,17 @@ import spacy
 import textacy
 import os
 import pandas as pd
+import re
+import string
+import nltk
+nltk.download('stopwords')
+nltk.download('punkt')
+from gensim import corpora, models, similarities
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+import pandas as pd
+import numpy as np
+
 from nltk.tokenize import sent_tokenize, word_tokenize
 new_list = []
 for root, dirs, files in os.walk("/Users/GowthamYesG/Desktop/KDM/Project/Abstracts"):
@@ -42,6 +53,48 @@ triplets= Remove(tuples_list)
 triplets = ''.join(str(e) for e in triplets)
 triplets = ''.join(str(e) for e in triplets)
 print("Triplets:", triplets)
+
+# def initial_clean(text):
+#     """
+#     Function to clean text-remove punctuations, lowercase text etc.
+#     """
+#     text = re.sub("[^a-zA-Z ]", "", text)
+#     text = text.lower()  # lower case text
+#     text = nltk.word_tokenize(text)
+#     return (text)
+#
+# stop_words = stopwords.words('english')
+# stop_words.extend(
+#     ['news', 'say', 'use', 'not', 'would', 'say', 'could', '_', 'be', 'know', 'good', 'go', 'get', 'do', 'took', 'time',
+#      'year',
+#      'done', 'try', 'many', 'some', 'nice', 'thank', 'think', 'see', 'rather', 'easy', 'easily', 'lot', 'lack', 'make',
+#      'want', 'seem', 'run', 'need', 'even', 'right', 'line', 'even', 'also', 'may', 'take', 'come', 'new', 'said',
+#      'like', 'people'])
+#
+# def remove_stop_words(text):
+#     return [word for word in text if word not in stop_words]
+#
+# stemmer = PorterStemmer()
+#
+# def stem_words(text):
+#     """
+#     Function to stem words
+#     """
+#     try:
+#         text = [stemmer.stem(word) for word in text]
+#         text = [word for word in text if len(word) > 1]  # no single letter words
+#     except IndexError:
+#         pass
+#
+#     return text
+#
+# def apply_all(text):
+#     """
+#     This function applies all the functions above into one
+#     """
+#     return stem_words(remove_stop_words(initial_clean(text)))
+
+
 
 f = open('output.txt','w')
 f.write(triplets)
